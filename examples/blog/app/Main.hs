5703853564bbbcb04de0f44c05e135ca3b0aa1c9
Chris Penner <christopher.penner@gmail.com>
2017-05-27 11:05:03 -0600

    Clean up blog example

--- examples/blog/app/Main.hs
+++ examples/blog/app/Main.hs
@@ -3,39 +3,40 @@
 module Main where
 
 import SitePipe
-import qualified Data.Map as M
-import Data.Maybe
+import qualified Text.Mustache as MT
+import qualified Text.Mustache.Types as MT
+import qualified Data.Text as T
 
 main :: IO ()
-main = site $ do
+main = siteWithGlobals templateFuncs $ do
   -- Load all the posts from site/posts/
-  posts' <- resourceLoader markdownReader ["posts/*.md"]
-  let tags = getTags makeTagUrl posts'
-      posts = addTagDataToPosts tags posts'
-  -- Create an object with the context for the table of contents
+  posts <- resourceLoader markdownReader ["posts/*.md"]
+  -- getTags will return a list of all tags from the posts,
+  -- each tag has a 'tag' and a 'posts' property
+  let tags = getTags makeTagUrl posts
+      -- Create an object with the needed context for a table of contents
       indexContext :: Value
       indexContext = object [ "posts" .= posts
                             , "tags" .= tags
                             , "url" .= ("/index.html" :: String)
                             ]
+
+  -- Render index page, posts and tags respectively
   writeTemplate "templates/index.html" [indexContext]
-  writeTemplate "templates/base.html" posts
+  writeTemplate "templates/post.html" posts
   writeTemplate "templates/tag.html" tags
   staticAssets
 
-addTagDataToPosts :: [Value] -> [Value] -> [Value]
-addTagDataToPosts tags posts = addTagData <$> posts
-  where
-    tagsByName = M.fromList (toKeyVal <$> tags)
-    toKeyVal tag = (tag ^. key "tag" . _String, tag)
-    lookupTag t = fromMaybe Null (tagsByName ^. at (t ^. _String))
-    addTagData post = over (key "tags" . values) lookupTag post
-
+-- We can provide a list of functions to be availabe in our mustache templates
+templateFuncs :: MT.Value
+templateFuncs = MT.object
+  [ "tagUrl" MT.~> MT.overText (T.pack . makeTagUrl . T.unpack)
+  ]
 
 makeTagUrl :: String -> String
 makeTagUrl tagName = "/tags/" ++ tagName ++ ".html"
 
--- | All the static assets can just be copied over
+-- | All the static assets can just be copied over from our site's source
 staticAssets :: SiteM ()
 staticAssets = copyFiles
     -- We can copy a glob
